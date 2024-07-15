11f4d2ad2dcd18fe791355a3b89a3ecd692ffbdb
UmiKami <ggoku02@gmail.com>
2024-07-14 22:39:51 -0400

    added more pages

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,6 +4,8 @@ import ForgotPassword from './views/ForgotPassword'
 import Login from './views/Login'
 import { Navbar } from './components/Navbar'
 import Register from './views/EmployeeRegister'
+import EmployeeManagement from './views/EmployeeManagement'
+import EmployeeDashboard from './views/EmployeeDashboard'
 
 function App() {
 
@@ -15,13 +17,12 @@ function App() {
         <Route path='/employee/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/reset-password' element />
-        <Route path='/admin/employee-management' element />
-        <Route path='/employee/dashboard' element />
+        <Route path='/admin/employee-management' element={<EmployeeManagement/>} />
+        <Route path='/employee/dashboard' element={<EmployeeDashboard/>} />
         <Route path='/employee/attendance' element />
         <Route path='/employee/deductions' element />
         <Route path='/employee/payroll' element />
-        <Route path='/employee/password-creation' element />
+        <Route path='/employee/password-reset' element />
         <Route path="*" element={<h1 className='text-4xl animate-bounce text-red-600 text-center font-black my-[35%]'>404 Not Found! <br/> <Link to={"/"} className='text-purple-600 underline'>Back to home page</Link> </h1>} />
       </Routes>
     </BrowserRouter>
