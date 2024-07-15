e30d2c608bbcce6c57fb639f178d5838b72d13e9
UmiKami <ggoku02@gmail.com>
2024-07-14 22:40:07 -0400

    added logout button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,22 @@
+import { Link } from "react-router-dom"
 
 export const Navbar = () => {
-  return (
-    <div className="w-100 px-2 bg-sky-400 text-white">
-          <ul className= "lg:w-[25%] md:w-[35%] sm:w-[50%] w-[100%] flex justify-between">
-            <li>User</li>
-            <li>Edit</li>
-            <li>Modules</li>
-            <li>Tools</li>
-            <li>Help</li>
-        </ul>
-    </div>
-  )
+
+    const handleClick = () => {
+        localStorage.removeItem("token")
+        localStorage.removeItem("role")
+    }
+
+    return (
+        <div className="w-100 flex justify-between px-2 bg-sky-400 text-white">
+            <ul className="lg:w-[25%] md:w-[35%] sm:w-[50%] w-[100%] flex justify-between">
+                <li>User</li>
+                <li>Edit</li>
+                <li>Modules</li>
+                <li>Tools</li>
+                <li>Help</li>
+            </ul>
+            <Link to={"/"} onClick={handleClick}>Log Out</Link>
+        </div>
+    )
 }
