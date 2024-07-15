cbf3182c85c785a5b3afe2f2d08e3baa4c7184dc
UmiKami <ggoku02@gmail.com>
2024-07-14 20:13:19 -0400

    minor style fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 
 export const Navbar = () => {
   return (
-    <div className="w-100 bg-sky-400 text-white">
+    <div className="w-100 px-2 bg-sky-400 text-white">
           <ul className= "lg:w-[25%] md:w-[35%] sm:w-[50%] w-[100%] flex justify-between">
             <li>User</li>
             <li>Edit</li>
