71f63aea936a6ea770f395791696b67eb76290b9
UmiKami <ggoku02@gmail.com>
2024-07-14 23:22:58 -0400

    Improved styles and added shortcut to payrolls

--- src/views/EmployeeDashboard.tsx
+++ src/views/EmployeeDashboard.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useEffect, useState } from "react"
 import axios from "../components/axiosConfig"
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 interface Payroll {
     payroll_id: string;
@@ -45,7 +45,7 @@ const EmployeeDashboard = () => {
             console.log(response)
             setEmployee(response.data)
         }).catch((error) => {
-            if (error.response.status === 401) {
+            if (error.response.status === 401 || error.response.status === 403) {
                 confirm("Unauthorized access. Please login.")
 
                 localStorage.removeItem("token")
@@ -82,8 +82,10 @@ const EmployeeDashboard = () => {
     }
 
     return (
-        <div className="p-6">
-            <form className="space-y-6" onSubmit={handleSubmit}>
+        <div className="flex flex-col items-center">
+            <h1 className="text-7xl w-[100%] mb-5 text-center font-bold bg-sky-600 p-[2vh] text-white text-shadow">PayTrack <br /><p className="text-base font-normal my-3">New User Registration</p></h1>
+
+            <form className="space-y-6 w-[80%] rounded-lg p-5 shadow-sm shadow-black" onSubmit={handleSubmit}>
                 <div className="grid grid-cols-2 gap-4">
                     <div>
                         <label htmlFor="employeeID" className="block text-sm font-medium text-gray-700">Employee ID</label>
@@ -109,14 +111,14 @@ const EmployeeDashboard = () => {
                 <button type="button" onClick={() => setIsEditable(true)} className="mt-4 text-blue-500 hover:underline">Update Information</button>
                 <hr className="my-4" />
                 <div className="text-lg font-bold">Leave Requests</div>
-                <div className="text-lg font-bold">Leave History</div>
-                <div className="text-lg font-bold">Create New Leave Request</div>
+                <Link to={""} className="text-lg font-medium text-sky-500">Leave History</Link>
+                <Link to={""} className="text-lg font-medium text-sky-500">Create New Leave Request</Link>
                 <hr className="my-4" />
                 <div className="text-lg font-bold">Payroll</div>
-                <div className="text-lg font-bold">View Payroll Records</div>
+                <Link to={"/payrolls"} className="text-lg font-medium  text-sky-500" >View Payroll Records</Link>
                 <hr className="my-4" />
-                <div className="text-lg font-bold">Attendance</div>
-                <div className="text-lg font-bold">View Attendance Records</div>
+                <div className="text-lg font-bold ">Attendance</div>
+                <Link to={""} className="text-lg font-medium text-sky-500">View Attendance Records</Link>
                 <div className="mt-6 flex justify-end space-x-4">
                     <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">Save</button>
                     <button type="button" onClick={() => setIsEditable(false)} className="bg-red-500 text-white px-4 py-2 rounded-md">Cancel</button>
