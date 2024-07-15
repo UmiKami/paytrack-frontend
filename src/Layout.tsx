32a6953b9fbfbd2a3a4f37a7b87240f61d45968a
UmiKami <ggoku02@gmail.com>
2024-07-14 20:10:27 -0400

    added default navbar and 404 page

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,24 +1,28 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import Home from './views/Home'
 import ForgotPassword from './views/ForgotPassword'
 import Login from './views/Login'
+import { Navbar } from './components/Navbar'
+import Register from './views/Register'
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/register' element={<Login/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/employee/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/reset-password' element/>
-        <Route path='/admin/employee-management' element/>
-        <Route path='/employee/dashboard' element/>
-        <Route path='/employee/attendance' element/>
-        <Route path='/employee/deductions' element/>
-        <Route path='/employee/payroll' element/>
-        <Route path='/employee/password-creation' element/>
+        <Route path='/reset-password' element />
+        <Route path='/admin/employee-management' element />
+        <Route path='/employee/dashboard' element />
+        <Route path='/employee/attendance' element />
+        <Route path='/employee/deductions' element />
+        <Route path='/employee/payroll' element />
+        <Route path='/employee/password-creation' element />
+        <Route path="*" element={<h1 className='text-4xl animate-bounce text-red-600 text-center font-black my-[35%]'>404 Not Found! <br/> <Link to={"/"} className='text-purple-600 underline'>Back to home page</Link> </h1>} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+
+export const Navbar = () => {
+  return (
+    <div className="w-100 bg-sky-400 text-white">
+          <ul className= "lg:w-[25%] md:w-[35%] sm:w-[50%] w-[100%] flex justify-between">
+            <li>User</li>
+            <li>Edit</li>
+            <li>Modules</li>
+            <li>Tools</li>
+            <li>Help</li>
+        </ul>
+    </div>
+  )
+}
